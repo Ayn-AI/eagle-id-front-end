9adeb7ac85fc9f33e42fe996d517f9bedf2ed0f2
Robin Ankele <robin@eyn.vision>
2020-08-13 16:22:59 +0100

    add demo project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import EagleId from 'eagle-id'
+
+function App() {
+  return (
+    <div className="App">
+        <EagleId />
+    </div>
+  );
+}
+
+export default App;
